ef7542950cbec7cd387c36dcee0a9947051cc99d
Noah Doersing <doersino@gmail.com>
2015-12-05 19:02:28 +0100

    Rework settitle() to set the title to the values of all arguments

--- settitle.sh
+++ settitle.sh
@@ -1,8 +1,3 @@
 function settitle() {
-    local USAGE
-    USAGE="usage: settitle WINDOW_TITLE"
-    if [ -z "$1" ]; then
-        echo -e "$USAGE"; return 1
-    fi
-    echo -ne "\033]0;$1\007"
+    echo -ne "\033]0;${@:1}\007"
 }
